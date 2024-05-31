f5d73d36c5c0cf2a3504ca894df0d0002dbb3941
massoum <massoummoncefpro@gmail.com>
2024-05-31 18:41:21 +0200

    prmier essay ya trp a changee

---
+++ index.js
@@ -0,0 +1,11 @@
+const incbut = document.getElementsByClassName("inc").onClick;
+const decbut = document.getElementsByClassName("dec").onClick;
+const reset = document.getElementsByClassName("reset").onclick;
+
+let num;
+num=  document.getElementsByClassName("input").onClick
+num=Number(num);
+function(){
+    document.getElementsByClassName("inc").onClick
+
+}
