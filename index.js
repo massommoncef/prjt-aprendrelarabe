d4a51a5c0a7e6179b4cf454e85ea3248a85654b7
massoum <massoummoncefpro@gmail.com>
2024-06-05 21:12:58 +0200

    project is working now

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
-const incbut = document.getElementsByClassName("inc");
-const decbut = document.getElementsByClassName("dec");
-const reset = document.getElementsByClassName("reset");
-const affichage = document.getElementsByClassName("affichage");
-let num=0;
+const incbut = document.getElementsByClassName("inc")[0];
+const decbut = document.getElementsByClassName("dec")[0];
+const resetbut = document.getElementsByClassName("reset")[0];
+const affichage = document.getElementsByClassName("affichage")[0];
+let num = 0;
 
-incbut.onclick = function(){
+incbut.onclick = function() {
     num++;
-   affichage.textContent = num ;
+    affichage.textContent = num;
 }
 
-
-decbut.onclick = function(){
+decbut.onclick = function() {
     num--;
-   affichage.textContent = num ;
-}
-incbut.onclick = function(){
-    num=0;
-   affichage.textContent = num ;
+    affichage.textContent = num;
 }
 
+resetbut.onclick = function() {
+    num = 0;
+    affichage.textContent = num;
+}
